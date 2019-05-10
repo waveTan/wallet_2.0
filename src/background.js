8a67fe430b100f3769876866f6fe791ec1bf3e81
wave <986887991@qq.com>
2019-05-10 10:39:30 +0800

    更新功能完成

--- src/background.js
+++ src/background.js
@@ -64,10 +64,10 @@ if (isDevelopment) {
 // 检测更新，在你想要检查更新的时候执行，renderer事件触发后的操作自行编写
 !function updateHandle() {
   let message = {
-    error: '检查更新出错',
-    checking: '正在检查更新……',
-    updateAva: '检测到新版本，正在下载……',
-    updateNotAva: '现在使用的就是最新版本，不用更新',
+    error: {type: 1, info: '检查更新出错'},
+    checking: {type: 2, info: '正在检查更新……'},
+    updateAva: {type: 3, info: '检测到新版本，正在下载……'},
+    updateNotAva: {type: 4, info: '现在使用的就是最新版本，不用更新'},
   };
   const uploadUrl = "http://192.168.1.119:8080/"; // 下载地址，不加后面的**.exe
   autoUpdater.setFeedURL(uploadUrl);
@@ -84,9 +84,7 @@ if (isDevelopment) {
     console.log(info);
     sendUpdateMessage(message.updateAva)
   });
-  autoUpdater.on('update-not-available', function (info) {
-    console.log("现在使用的就是最新版本，不用更新:");
-    console.log(info);
+  autoUpdater.on('update-not-available', function () {
     sendUpdateMessage(message.updateNotAva)
   });
 
--- src/components/BottomBar.vue
+++ src/components/BottomBar.vue
@@ -1,19 +1,37 @@
 <template>
+  <div>
     <div class="bottom">
-        <div class="w1200 font14">
-            <div class="left fl">
-                <p class="fl">节点服务器: <u class="clicks" @click="toUrl('nodeService')">{{serviceUrls}}</u></p>
-                <!--<p class="fr">高度:节点131000000/主网 {{heightInfo.height}}</p>-->
-                <p class="fr">主网高度: {{heightInfo.height}}</p>
-            </div>
-            <div class="right fr">
-                <label class="clicks">服务协议</label>
-                <label class="clicks">隐私政策</label>
-                <label>V 2.0.1-Alpha</label>
-                <!-- <label class="click" @click="checkUpdate">Alpha 2.0.2</label>-->
-            </div>
+      <div class="w1200 font14">
+        <div class="left fl">
+          <p class="fl">服务节点URL: <u class="clicks" @click="toUrl('nodeService')">{{serviceUrls}}</u></p>
+          <!--<p class="fr">高度:节点131000000/主网 {{heightInfo.height}}</p>-->
+          <p class="fr">主网/服务节点: {{heightInfo.height}}/{{heightInfo.height}}</p>
         </div>
+        <div class="right fr">
+          <label class="clicks">服务协议</label>
+          <label class="clicks">隐私政策</label>
+          <!--<label>2.0.1-alpha</label>-->
+          <label class="click" @click="checkUpdate">Alpha 2.0.1</label>
+        </div>
+      </div>
     </div>
+    <el-dialog title="更新钱包" width="35rem"
+               :visible.sync="updateDialogVisible"
+               :show-close="tips.type===4"
+               :close-on-press-escape="false"
+               :close-on-click-modal="false">
+      <div class="upload">
+        <div class="upload-tips">提示信息：{{tips.info}}</div>
+        <div class="upload-percent" v-if="downloadPercent !==0 ">
+          <el-progress :text-inside="true" :stroke-width="18" :percentage="downloadPercent" status="success"></el-progress>
+        </div>
+        <div class="upload-bt" v-if="tips.type === 3">
+          <el-button type="info" @clcik="afterRun">后台运行</el-button>
+        </div>
+      </div>
+    </el-dialog>
+  </div>
+
 </template>
 
 <script>
@@ -22,7 +40,10 @@
     data() {
       return {
         heightInfo: [],//最新高度
-        serviceUrls: localStorage.hasOwnProperty("urls") ? JSON.parse(localStorage.getItem("urls")).urls : 'http://192.168.1.192:18003/'
+        serviceUrls: localStorage.hasOwnProperty("urls") ? JSON.parse(localStorage.getItem("urls")).urls : 'http://192.168.1.192:18003/',
+        updateDialogVisible:false,//更新弹框
+        tips:{},//提示信息
+        downloadPercent:0,//下载进度
       }
     },
     created() {
@@ -71,22 +92,27 @@
        * 检查更新
        **/
       async checkUpdate() {
+        this.updateDialogVisible =true;
+        this.tips={};
+        this.downloadPercent =0;
         const _this = this;
         _this.$electron.ipcRenderer.send("checkForUpdate");
         await _this.$electron.ipcRenderer.on("message", (event, text) => {
-          console.log(arguments);
           _this.tips = text;
-          console.log(text);
-          //alert(text)
         });
         _this.$electron.ipcRenderer.on("downloadProgress", (event, progressObj) => {
-          console.log(event);
-          console.log(progressObj);
           _this.downloadPercent = progressObj.percent || 0;
         });
         _this.$electron.ipcRenderer.on("isUpdateNow", () => {
           _this.$electron.ipcRenderer.send("isUpdateNow");
         });
+      },
+
+      /**
+       * 后台运行
+       **/
+      afterRun(){
+        this.updateDialogVisible =false;
       },
 
       /**
@@ -99,58 +125,58 @@
           name: name
         })
       },
-
     }
   }
 </script>
 
 <style lang="less">
-    @import "./../assets/css/style";
+  @import "./../assets/css/style";
 
-    .bottom {
-        height: 60px;
-        border-top: 1px solid @Dcolour;
-        position: fixed;
+  .bottom {
+    height: 60px;
+    border-top: 1px solid @Dcolour;
+    position: fixed;
+    line-height: 60px;
+    bottom: 0;
+    background-color: @Bcolour;
+    width: 100%;
+    @media screen and (max-width: 1000px) {
+      height: 4rem;
+      font-size: 0.7rem;
+      line-height: 1rem;
+    }
+    .w1200 {
+      .left {
+        width: 50%;
         line-height: 60px;
-        bottom: 0;
-        background-color: @Bcolour;
-        width: 100%;
         @media screen and (max-width: 1000px) {
-            height: 4rem;
+          width: 100%;
+          margin: 0.5rem 0 0 0.5rem;
+          p {
+            margin: 0;
+            float: none;
+            display: block;
+          }
+        }
+      }
+      .right {
+        width: 40%;
+        line-height: 60px;
+        text-align: right;
+        @media screen and (max-width: 1000px) {
+          width: 100%;
+          float: none;
+        }
+        label {
+          padding: 0 10px;
+          @media screen and (max-width: 1000px) {
+            padding: 0 0.5rem;
             font-size: 0.7rem;
             line-height: 1rem;
+          }
         }
-        .w1200 {
-            .left {
-                width: 50%;
-                line-height: 60px;
-                @media screen and (max-width: 1000px) {
-                    width: 100%;
-                    margin: 0.5rem 0 0 0.5rem;
-                    p {
-                        margin: 0;
-                        float: none;
-                        display: block;
-                    }
-                }
-            }
-            .right {
-                width: 40%;
-                line-height: 60px;
-                text-align: right;
-                @media screen and (max-width: 1000px) {
-                    width: 100%;
-                    float: none;
-                }
-                label {
-                    padding: 0 10px;
-                    @media screen and (max-width: 1000px) {
-                        padding: 0 0.5rem;
-                        font-size: 0.7rem;
-                        line-height: 1rem;
-                    }
-                }
-            }
-        }
+      }
     }
+
+  }
 </style>
